d4bdce34ae9861f0492d999184271aa098dfc13e
zeroet <y36609672@gmail.com>
2022-04-03 00:01:19 +0200

    key button

---
+++ tuto01/Assets/Anykey.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anykey : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            Debug.Log("플레이어  랜덤키 입력");    
+    }
+}
--- tuto01/Assets/NewCycle.cs
+++ tuto01/Assets/NewCycle.cs
@@ -9,6 +9,11 @@ public class NewCycle : MonoBehaviour
         Debug.Log("Player Data is Init!");
     }
 
+    void    OnEnable()
+    {
+        Debug.Log("Player Login !!!");
+    }
+
     void    Start()
     {
         Debug.Log("Player take the weapon!");
@@ -21,6 +26,44 @@ public class NewCycle : MonoBehaviour
     }
 
     // Game logic area
+
+    void    Update()
+    {
+       if (Input.anyKeyDown)
+        Debug.Log("플레이어 키 입력");
+
+       if (Input.GetKeyDown(KeyCode.Return))
+        Debug.Log("아이템을 구입");
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        Debug.Log("왼쪽으로 이동중");
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        Debug.Log("오른쪽으로 이동");
     
+        if (Input.GetMouseButton(0))
+            Debug.Log("미사일 모으는 중 ");
+
+        if (Input.GetMouseButtonUp(0))
+        Debug.Log("미사일 발사 !");
+
+
+    }
+
+    void    LastUpdate()
+    {
+        Debug.Log("level up!");
+    }
+
+    void    OnDisable()
+    {
+        Debug.Log("Player LogOut");
+
+    }
+
+    void    OnDestroy()
+    {
+        Debug.Log("end game");
+    }
 
 }
