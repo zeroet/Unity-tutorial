12847a1ec510121578bebcc46fd9348e11393a5c
zeroet <y36609672@gmail.com>
2022-04-21 01:40:36 +0200

    sound

---
+++ B.B/Assets/Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float rotatespeed;
+    
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotatespeed * Time.deltaTime, Space.World);      
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Myball player = other.GetComponent<Myball>();
+            audio.Play();
+            gameObject.SetActive(false);
+            player.Score++;
+        }
+    }
+}
---
+++ B.B/Assets/Myball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Myball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Score;
+    public float jumpPower;
+    bool isjump;
+    Rigidbody rigid;
+    void Awake()
+    {
+       rigid = GetComponent<Rigidbody>();
+       isjump = false;
+    }
+
+    // Update is called once per frame
+    void  Update()
+    {
+        if(Input.GetButtonDown("Jump") && !isjump)
+         {   
+            isjump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+         }
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(h, 0, v);
+        rigid.AddForce(vec, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+            isjump = false;
+    }
+}
